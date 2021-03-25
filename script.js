7d072f2c0bef4a6b376feba2bae2aef732b9cf5a
Patrick <tobey.tibet123@gmail.com>
2021-03-25 01:13:22 -0400

    done for the most part

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ myButton.addEventListener('click', () => {
 
 for (let i = 0; i < (gridSize**2); i++) {
     let test = 100/gridSize
-    console.log(test)
+    
     const createDiv = document.createElement('div')
     totalBoxes = parseInt(i) + 1
     createDiv.classList.add(`box`) //do i need to change the class of every box made?
-    createDiv.textContent = `box ${totalBoxes}`
+    
     createDiv.style.width = `${test}%`
     createDiv.style.height = createDiv.style.width
     createDiv.style.verticalAlign = 'middle'
@@ -44,10 +44,11 @@ const changeColour = document.querySelectorAll('.box')
 for (let i=0; i < changeColour.length; i++)
     changeColour[i].addEventListener('mouseenter', () => {
         
-        changeColour[i].style.background = `rgb(${r}, ${g}, ${b})`
-        r -= 25.5
-        g -= 25.5
-        b -= 25.5
+        // changeColour[i].style.background = `rgb(${r}, ${g}, ${b})`
+        // r -= 25.5
+        // g -= 25.5
+        // b -= 25.5
+
         // for (let j = 0; j < 10; j++) {
         //     changeColour[i].style.background = `rgb(${r}, ${g}, ${b})`
         //     r -= 25.5
@@ -55,8 +56,10 @@ for (let i=0; i < changeColour.length; i++)
         //     b -= 25.5
             
         // }  
+
+
         // changeColour[i].style.background = 'black'
-        // changeColour[i].style.background = "#" + Math.floor(Math.random()*16777215).toString(16) changes it to random colours
+        changeColour[i].style.background = "#" + Math.floor(Math.random()*16777215).toString(16) //changes it to random colours
     })
 
 
